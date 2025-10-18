b19cd6c865dcd4f68ac4b2da6bdb8ec882b8b2f6
Sayemahamed <sayemahamed183@gmail.com>
2025-10-19 05:32:41 +0600

    Implemented Moving averages calculation

--- main.py
+++ main.py
@@ -18,32 +18,42 @@ class TradingBot:
         self.data:pd.DataFrame|None=None
         self.trades=[]
     def fetch_data(self)->bool:
-        console = Console()
-        console.print(f"[bold blue]Fetching data for {self.symbol} from {self.start_date} to {self.end_date}")
+        Console().print(f"[bold blue]Fetching data for {self.symbol} from {self.start_date} to {self.end_date}")
         try:
             self.data = yf.download(tickers=self.symbol, start=self.start_date, end=self.end_date,auto_adjust=True)
-            console.print(f"[bold green]Data fetched for {self.symbol}")
+            Console().print(f"[bold green]Data fetched for {self.symbol}")
         except Exception as e:
-            console.print(f"[bold red]Error fetching data for {self.symbol}: {e}")
+            Console().print(f"[bold red]Error fetching data for {self.symbol}: {e}")
             return False
         if self.data is None or self.data.empty:
-            console.print(f"[yellow]No data found for {self.symbol} between {self.start_date} and {self.end_date}")
+            Console().print(f"[yellow]No data found for {self.symbol} between {self.start_date} and {self.end_date}")
             return False
         self.data.drop_duplicates(inplace=True)
         self.data.ffill(inplace=True)
-        console.print("[bold green]Data Processed Successfully")
+        Console().print("[bold green]Data Processed Successfully")
         return True
     
+    def calculate_moving_average(self)->bool:
+        Console().print("[bold blue]Calculating Moving Average")
+        if self.data is None or self.data.empty:
+            Console().print(f"[yellow]No data found for {self.symbol} between {self.start_date} and {self.end_date}")
+            return False
+        self.data["MA50"] = self.data["Close"].rolling(window=50).mean()
+        self.data["MA200"] = self.data["Close"].rolling(window=200).mean()
+        Console().print("[bold green]Moving Average Calculated Successfully")
+        return True
 
 
 
 
 if __name__ == "__main__":
-    trading_bot = TradingBot(
+    trading_bot:TradingBot = TradingBot(
         symbol="AMZN",
         start_date="2018-01-01",
         end_date="2023-12-31"
     )
     
-    if trading_bot.fetch_data():
+    if trading_bot.fetch_data() and trading_bot.calculate_moving_average():
+        pass
+    else:
         pass
